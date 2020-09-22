42c27318f0b815ca4e9a96aeeaa5b9cf06337da3
Ulrich Schmidt-Goertz <ulrich.schmidt-goertz@finanzcheck.de>
2020-09-22 16:49:59 +0200

    Allow aliasing the table name (#3)

--- tests/CompositeNumberRangeBehaviorTest.php
+++ tests/CompositeNumberRangeBehaviorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Propel\Tests\Generator\Behavior\CompositeNumberRange;
+namespace Finanzcheck\CompositeNumberRange;
 
 use Finanzcheck\CompositeNumberRange\Platform\MysqlPlatform;
 use Propel\Generator\Util\QuickBuilder;
@@ -189,6 +189,61 @@ EOF;
         $child->reload();
 
         $this->assertGreaterThan($firstParentTableId, $child->getParentTableId());
+    }
+
+    public function testUsesTheLocalTableAliasParameterWhenGiven()
+    {
+        $schema = <<<EOF
+<database name="composite_number_range_test">
+    <table name="foo">
+        <column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+    </table>
+    <table name="bar">
+        <column name="id" required="true" primaryKey="true" autoIncrement="true" type="INTEGER" />
+        <behavior name="\Finanzcheck\CompositeNumberRange\CompositeNumberRangeBehavior">
+            <parameter name="foreignTable" value="foo"/>
+            <parameter name="localTableAlias" value="baz"/>
+        </behavior>
+    </table>
+</database>
+EOF;
+
+        $builder = new QuickBuilder();
+        $builder->setPlatform(new MysqlPlatform());
+        $builder->setSchema($schema);
+        $builder->build(
+            'mysql:host=127.0.0.1;dbname=' . getenv('DB_NAME'),
+            getenv('DB_USER'),
+            getenv('DB_PASS'),
+            new MysqlAdapter()
+        );
+
+        $this->assertTrue(class_exists('\Bar'));
+        $this->assertTrue(class_exists('\BarQuery'));
+        $this->assertTrue(class_exists('\Foo'));
+        $this->assertTrue(class_exists('\FooQuery'));
+        $this->assertTrue(class_exists('\FooSequence'));
+        $this->assertTrue(class_exists('\FooSequenceQuery'));
+
+        $entity = new \Bar();
+
+        $this->assertTrue(method_exists($entity, 'setFoo'));
+        $this->assertTrue(method_exists($entity, 'setFooId'));
+        $this->assertTrue(method_exists($entity, 'setFooBazId'));
+
+        $sequenceEntity = new \FooSequence();
+
+        $this->assertTrue(method_exists($sequenceEntity, 'setTableName'));
+        $this->assertTrue(method_exists($sequenceEntity, 'setFooId'));
+        $this->assertTrue(method_exists($sequenceEntity, 'setFooMaxSequenceId'));
+
+        $foo = new \Foo();
+        $foo->save();
+
+        $entity->setFoo($foo);
+        $entity->save();
+
+        $this->assertEquals($foo->getId(), $entity->getFooBazId());
     }
 }
  
